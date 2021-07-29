aafb54e6c9d5369b9d6745fb1ecf289b581c0b81
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-29 14:23:56 -0700

    Add a new instance of someone having the same age and life expectancy.

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -4,18 +4,18 @@ describe('Calculator', () => {
   let calculator;
 
   beforeEach(() => {
-    calculator = new Calculator( 25,'MARS', 65);
+    calculator = new Calculator( 65,'MARS', 65);
   });
   
   test('should be able to hold three parameters: given earth age, the selected planet, the users life expectancy', () => {
-    expect(calculator.earthAge).toEqual(25);
+    expect(calculator.earthAge).toEqual(65);
     expect(calculator.selectedPlanet).toEqual('MARS');
     expect(calculator.userLifeExpectancy).toEqual(65);
   });
   test('should correctly return calculated age of given planet', () => {
-    expect(calculator.alienAge()).toEqual(47.0525);
+    expect(calculator.alienAge()).toEqual(122.3365);
   })
   test('should inform user of how many years they would have left or how many they lived past expectation on a given planet', () => {
-    expect(calculator.deathYear()).toEqual(75.284);
+    expect(calculator.deathYear()).toEqual("You better watch it bud. You're livin' on borrowed time!");
   })
 });
