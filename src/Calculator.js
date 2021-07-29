c1c00cc9512d4ccb89e8b6862d4c5f48b217c8fb
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-29 12:49:12 -0700

    Create a prototype that passes the second test by giving the planet age.

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,6 +3,24 @@ export default class Calculator {
     this.earthAge = earthAge;
     this.selectedPlanet = selectedPlanet;
     this.userLifeExpectancy = userLifeExpectancy;
-
+  }
+  alienAge = () => {
+    if (this.selectedPlanet === 'MERCURY'){
+      let mercuryAge = (this.earthAge * .241);
+      return mercuryAge;
+      //return ('You are ' + mercuryAge + ' on Mercury.')
+    } else if (this.selectedPlanet === 'VENUS'){
+      let venusAge = (this.earthAge * .6164);
+      return venusAge;
+      //return ('You are ' + venusAge + ' on Venus.');
+    } else if (this.selectedPlanet === 'MARS'){
+      let marsAge = (this.earthAge * 1.8821);
+      return marsAge;
+      //return ('You are ' + marsAge + ' on Mars.'); 
+    } else if (this.selectedPlanet === 'JUPITER'){
+      let jupiterAge = (this.earthAge * 12);
+      return jupiterAge;
+      //return ('You are ' + jupiterAge + ' on Jupiter.')
+    }
   }
 }
