042101c02cfd73f29709e1db6542d1a88a74acab
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-29 12:01:50 -0700

    Add a constructor that passes our intial test.

---
+++ src/Calculator.js
@@ -0,0 +1,8 @@
+export default class Calculator {
+  constructor(earthAge, selectedPlanet, userLifeExpectancy) {
+    this.earthAge = earthAge;
+    this.selectedPlanet = selectedPlanet;
+    this.userLifeExpectancy = userLifeExpectancy;
+
+  }
+}
