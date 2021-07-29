172445f52d6cfc3a2e29245615200891819199fb
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-29 12:47:55 -0700

    tests that the prototype will calculate a planet age.

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -4,13 +4,15 @@ describe('Calculator', () => {
   let calculator;
 
   beforeEach(() => {
-    calculator = new Calculator( 25,'Mars', 65);
+    calculator = new Calculator( 25,'MARS', 65);
   });
   
   test('should be able to hold three parameters: given earth age, the selected planet, the users life expectancy', () => {
     expect(calculator.earthAge).toEqual(25);
-    expect(calculator.selectedPlanet).toEqual('Mars');
+    expect(calculator.selectedPlanet).toEqual('MARS');
     expect(calculator.userLifeExpectancy).toEqual(65);
-
   });
+  test('should correctly return calculated age of given planet', () => {
+    expect(calculator.alienAge()).toEqual(47.0525);
+  })
 });
