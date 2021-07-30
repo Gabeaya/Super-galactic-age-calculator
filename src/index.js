633cd3e19cf32aef487c5ed323f8b5f27652558f
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-30 14:08:13 -0700

    Add a form input.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-/*import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import any business logic with the following syntax:
-import [BusinessObject] from './triangle.js';
-for multiple imports from the same file follow this syntax:
-import[{*/
 
+$(document).ready(function() {
+  $("form#userAge").submit(function(e) {
+    e.preventDefault();
+    $(".age-page").hide();
+    $(".planet-page").show();
+  
+  });
+});
 
 
