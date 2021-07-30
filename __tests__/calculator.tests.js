d815482fec0966758a20e51faaa7ff87c6635843
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-30 16:46:20 -0700

    Make changes to account for life expect to be 75.

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -4,16 +4,16 @@ describe('Calculator', () => {
   let calculator;
 
   beforeEach(() => {
-    calculator = new Calculator( 65,'MARS', 65);
+    calculator = new Calculator( 75,'MARS', 75);
   });
   
   test('should be able to hold three parameters: given earth age, the selected planet, the users life expectancy', () => {
-    expect(calculator.earthAge).toEqual(65);
+    expect(calculator.earthAge).toEqual(75);
     expect(calculator.selectedPlanet).toEqual('MARS');
-    expect(calculator.userLifeExpectancy).toEqual(65);
+    expect(calculator.userLifeExpectancy).toEqual(75);
   });
   test('should correctly return calculated age of given planet', () => {
-    expect(calculator.alienAge()).toEqual(122.3365);
+    expect(calculator.alienAge()).toEqual(141.1575);
   })
   test('should inform user of how many years they would have left or how many they lived past expectation on a given planet', () => {
     expect(calculator.deathYear()).toEqual("You better watch it bud. You're livin' on borrowed time!");
