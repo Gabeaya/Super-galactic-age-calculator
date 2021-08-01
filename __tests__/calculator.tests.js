8e8510b693376350e4c30f9534fef5533ded54c3
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-31 19:53:13 -0700

    Create a test for age when mars is selected.

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -8,6 +8,7 @@ describe('Calculator', () => {
     calculator = new Calculator('','','');
     marsCalculator = new Calculator( 75,'MARS', 75);
 
+
   });
   
   test('should be able to hold three parameters: given earth age, the selected planet, the users life expectancy', () => {
@@ -15,7 +16,7 @@ describe('Calculator', () => {
     expect(calculator.selectedPlanet).toEqual('');
     expect(calculator.userLifeExpectancy).toEqual('');
   });
-  test('should correctly return calculated age of given planet', () => {
+  test('should correctly return calculated age of planet a person on Mars', () => {
     expect(marsCalculator.alienAge()).toEqual(39.84910472344721);
   })
   test('should inform user of how many years they would have left or how many they lived past expectation on a given planet', () => {
