ebea234dfd52a14dd266ab354bc716a5425f0b36
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-31 20:54:34 -0700

    use mathfloor to get whole numbers.

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,26 +6,26 @@ export default class Calculator {
   }
   alienAge = () => {
     if (this.selectedPlanet === 'MERCURY'){
-      let mercuryAge = (this.earthAge / .241);
+      let mercuryAge = Math.floor(this.earthAge / .241);
       return mercuryAge;
     } else if (this.selectedPlanet === 'VENUS'){
-      let venusAge = (this.earthAge / .6164);
+      let venusAge = Math.floor(this.earthAge / .6164);
       return venusAge;
     } else if (this.selectedPlanet === 'MARS'){
-      let marsAge = (this.earthAge / 1.8821);
+      let marsAge = Math.floor(this.earthAge / 1.8821);
       return marsAge;
     } else if (this.selectedPlanet === 'JUPITER'){
-      let jupiterAge = (this.earthAge / 12);
+      let jupiterAge = Math.floor(this.earthAge / 12);
       return jupiterAge;
     }
   }
   deathYear = () => {
     if(this.selectedPlanet === 'MERCURY'){
       if(this.earthAge < this.userLifeExpectancy) {
-        let yearsExpected = ((this.userLifeExpectancy - this.earthAge) / .241);
+        let yearsExpected = Math.floor((this.userLifeExpectancy - this.earthAge) / .241);
         return yearsExpected;
       } else if(this.earthAge > this.userLifeExpectancy) {
-        let yearsPastExpected = ((this.earthAge - this.userLifeExpectancy) / .241);
+        let yearsPastExpected = Math.floor((this.earthAge - this.userLifeExpectancy) / .241);
         return yearsPastExpected;
       } else if(this.earthAge === this.userLifeExpectancy) {
         const warning = "You better watch it bud. You're livin' on borrowed time!";
@@ -33,10 +33,10 @@ export default class Calculator {
       }
     } else if(this.selectedPlanet === 'VENUS'){
       if(this.earthAge < this.userLifeExpectancy) {
-        let yearsExpected = ((this.userLifeExpectancy - this.earthAge) / .6164);
+        let yearsExpected = Math.floor((this.userLifeExpectancy - this.earthAge) / .6164);
         return yearsExpected;
       } else if(this.earthAge > this.userLifeExpectancy) {
-        let yearsPastExpected = ((this.earthAge - this.userLifeExpectancy) / .6164);
+        let yearsPastExpected = Math.floor((this.earthAge - this.userLifeExpectancy) / .6164);
         return yearsPastExpected;
       } else if(this.earthAge === this.userLifeExpectancy) {
         const warning = "You better watch it bud. You're livin' on borrowed time!";
@@ -44,10 +44,10 @@ export default class Calculator {
       }
     } else if(this.selectedPlanet === 'MARS'){
       if(this.earthAge < this.userLifeExpectancy) {
-        let yearsExpected = ((this.userLifeExpectancy - this.earthAge) / 1.8821);
+        let yearsExpected = Math.floor((this.userLifeExpectancy - this.earthAge) / 1.8821);
         return yearsExpected;
       } else if(this.earthAge > this.userLifeExpectancy) {
-        let yearsPastExpected = ((this.earthAge - this.userLifeExpectancy) / 1.8821);
+        let yearsPastExpected = Math.floor((this.earthAge - this.userLifeExpectancy) / 1.8821);
         return yearsPastExpected;
       } else if(this.earthAge === this.userLifeExpectancy) {
         const warning = "You better watch it bud. You're livin' on borrowed time!";
@@ -55,10 +55,10 @@ export default class Calculator {
       }
     } else if(this.selectedPlanet === 'JUPITER'){
       if(this.earthAge < this.userLifeExpectancy) {
-        let yearsExpected = ((this.userLifeExpectancy - this.earthAge) / 11.86);
+        let yearsExpected = Math.floor((this.userLifeExpectancy - this.earthAge) / 11.86);
         return yearsExpected;
       } else if(this.earthAge > this.userLifeExpectancy) {
-        let yearsPastExpected = ((this.earthAge - this.userLifeExpectancy) / 11.86);
+        let yearsPastExpected = Math.floor((this.earthAge - this.userLifeExpectancy) / 11.86);
         return yearsPastExpected;
       } else if(this.earthAge === this.userLifeExpectancy) {
         const warning = "You better watch it bud. You're livin' on borrowed time!";
